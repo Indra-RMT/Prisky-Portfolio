cc1d247aa5629fa3258c3e3243c3db2264716e76
indra <indra.anto42@gmail.com>
2020-12-07 20:17:40 +0700

    add home page

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const initPageHandler = (currentPage) => {
 		if (currentPage === '/home') {
+			console.log('init')
 			initHomePage();
 		}
 	} 
