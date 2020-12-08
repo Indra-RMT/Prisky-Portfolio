74f950be1566f1ab021bcdb272582f855f92e422
indra <indra.anto42@gmail.com>
2020-12-08 14:20:01 +0700

    add detail angel-skin-clinic

--- js/data/portfolioData.js
+++ js/data/portfolioData.js
@@ -1,13 +1,89 @@
 export const portfolioData = [
   {
-    slug: 'angel',
-    name: 'ui/ux',
+    slug: 'angel-skin-clinic',
+    name: 'Angel Skin Clinic',
     image: 'angel.png',
     category: 'study case',
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    text: `
+    <p><b>Platform:</b></p>
+    <p>Android</p>
+    
+    <p><b>Role:</b></p>
+    <p>UX Designer</p>
+    
+    <p>&nbsp;</p>
+    <p>Angel Skin Clinic adalah case study pertama saya. Case study ini terinspirasi dari pengalaman saya melakukan konsultasi dokter di salah satu klinik kecantikan. Dari situlah saya mulai mencari tahu kira-kira apa masalah yang saya alami ketika melakukan konsultasi.</p>
+    
+    <h4>Step 1: Emphatize</h4>
+    <p><b>Saya sebagai pasien</b></p>
+    <p>Sebagai pasien saya mulai menjabarkan kesulitan-kesulitan yang saya alami, hal yang membuat saya frustasi, dan hal yang mengganggu atau bahkan menghambat saya. Pada tahap ini saya mencoba untuk berada pada posisi pasien, bukan menjadi seorang desainer.</p>
+    <p>Dan ini adalah beberapa poin initial hipotesis yang saya temukan:</p>
+    <p>- Pasien yang ke klinik kecantikan adalah orang yang mengalami masalah kulit dan berharap dapat menyelesaikan masalah kulit dengan berkonsultasi kepada dokter.</p>
+    <p>- Pasien yang datang ke klinik kecantikan adalah orang yang merawat penampilan agar memiliki kulit yang sehat dan terawat.</p>
+    <p>- Pelayanan masih dilakukan secara manual sehingga kurang efisien apalagi untuk pasien yang tidak bisa datang langsung ke klinik karena alasan tertentu.</p>
+    
+    <p><b>User needs</b></p>
+    <p>Kemudian saya mencoba untuk mencari tahu apakah pasien lain mengalami hal serupa. Untuk itu saya melakukan research dengan mencari pengalaman pasien di klinik kecantikan di blog, tanggapan atau comment pada blog, forum, google review, maupun pada media sosial.</p>
+    <p>Ini adalah beberapa yang saya temukan:</p>
+    <p><i>“antriannya lama banget tapi tetap tunggu loh”</i>- blog</p>
+    <p><i>“sistemnya by phone tapi susah nyambungnya karena sibuk terus”- google review</i></p>
+    <p><i>“ngga boleh booking untuk 2 minggu atau lebih”</i>- google review</p>
+    
+    <p><b>User Interview</b></p>
+    <p>Meskipun saya telah mendapat banyak referensi dari review melalui internet, saya memutuskan untuk melakukan interview agar mendapat informasi lebih detail.</p>
+    
+    <h4>Step 2: Define</h4>
+    <p>Setelah melakukan research dan mendapatkan berbagai informasi, saya mulai menyusun data yang saya peroleh. Selanjutnya saya mengelompokkan data tersebut sesuai jenis permasalahan yang dialami pasien.</p>
+    
+    <img src="../../public/img/detail-ui-ux-1.png" alt="define">
+    
+    <p>Aku mengelompokkan apa yang menjadi kesulitan-kesulitan dan permasalahan yang mereka alami dengan membaginya menjadi 3 bagian, sebelum konsultasi, saat konsultasi, dan setelah konsultasi.</p>
+    
+    <p><b>Personas</b></p>
+    <p>Setelah mendapatkan referensi dari pasien dan belajar tentang apa yang menjadi masalah mereka, selanjutnya saya mencoba membuat persona untuk merepresentasikan pasien.</p>
 
-    Why do we use it?
-    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`
+    <img class="width-80" src="../../public/img/detail-ui-ux-2.png" alt="personas 1">
+    <img class="width-80" src="../../public/img/detail-ui-ux-3.png" alt="personas 2">
+    
+    <p>Clara merepresentasikan pasien yang datang ke klinik karena memiliki masalah kulit dan ingin menyembuhkan permasalahan kulitnya. Sedangkan Karina adalah pasien yang datang ke klinik untuk tujuan estetik, agar kulit sehat dan terawat.</p>
+
+    <p><b>Define the Problem</b></p>
+    <p>Setelah mendapatkan pemahaman yang clear tentang ‘Siapa’ yang akan saya bantu untuk menyelesaikan masalah, saya mulai beralih untuk mencari tahu apa yang mereka butuhkan. Saya mulai membuat user need statement.</p>
+
+    <p>User need statement:</p>
+    <p><i>“saya ingin pergi ke klinik, tapi antrian yang panjang membuat saya malas dan enggan pergi.”</i></p>
+    <p><i>“saya ingin melakukan konsultasi dengan dokter, tapi saya tidak bisa mengetahui jadwal praktek dokter secara pasti.”</i></p>
+    <p><i>“saya ingin konsultasi dengan dokter, tapi proses administrasi yang lama membuat saya harus menghabiskan banyak waktu. ”</i></p>
+    <p><i>“saya hanya ingin melakukan pembelian produk yang telah habis tanpa konsultasi, tapi saya harus datang langsung ke klinik padahal saya tidak memiliki banyak waktu.”</i></p>
+    <p>Kemudian dari user need statement diatas , saya mencoba untuk membuat POV untuk case study ini:</p>
+    <p><i>“Bagaimana jika saya membuat aplikasi secara online untuk membantu pasien melakukan konsultasi dengan mudah dan efisien.”</i></p>
+    <p>Pasien akan dengan mudah melakukan konsultasi dengan dokter tanpa harus bertatap muka secara langsung. Pasien dapat melakukan pembuatan janji dengan dokter sesuai dengan preferensi waktu tertentu. Pasien dapat melakukan pembelian produk dengan mudah dan akan dikirim langsung ke rumah pasien tanpa harus datang ke klinik.</p>
+    
+
+    <h4>Step 3: Ideate</h4>
+    <p>Selanjutnya saya mulai mencari tahu apa masalah terbesar dan urgent untuk diselesaikan terlebih dahulu.</p>
+    <p><b>Sketch</b></p>
+    <p>Tahap selanjutnya adalah menuliskan langkah-langkah yang dilakukan user ketika menggunakan aplikasi. Dibawah ini adalah flow ketika user melakukan pembuatan janji untuk melakukan konsultasi:</p>
+
+    <img class="width-80" src="../../public/img/detail-ui-ux-4.png" alt="user flow pembuatan janji">
+
+    <p>Selanjutnya adalah membuat Architecture Information untuk aplikasi:</p>
+
+    <img class="width-80" src="../../public/img/detail-ui-ux-5.png" alt="architecture information aplikasi">
+
+
+    <h4>Step 4: Prototype</h4>
+    <p><b>Paper prototype</b></p>
+    <p>Selanjutnya membuat prototype berdasarkan user flow yang telah di buat sebelumnya. Pembuatan prototype menggunakan kertas dan pulpen.</p>
+
+    <p><b>Digital Prototype - Low Fidelity</b></p>
+    <p>Setelah dirasa flow telah sesuai, kemudian beralih dengan membuat digital prototype.
+    Kemudian membuat interaksi ketika user menggunakan aplikasi.</p>
+
+    <img src="../../public/img/detail-ui-ux-6.png" alt="Digital Prototype - Low Fidelity">
+
+    <p><b>Branding</b></p>
+    <p>Pada tahap ini dilakukan pembuatan logo, pemilihan warna, typography, dan iconography.</p>`
   },
   {
     slug: 'onboarding',
--- js/pages/detail.js
+++ js/pages/detail.js
@@ -5,13 +5,15 @@ export const initDetailPage = (slug) => {
 
   const mainWrapper = document.querySelector('#main');
   let htmlDetail = '';
-  const htmlImage = `<img src="../../public/img/${portfolioDetail.image}" alt="${portfolioDetail.name}"/>`;
+  let htmlImage = `<img src="../../public/img/${portfolioDetail.image}" alt="${portfolioDetail.name}"/>`;
   const htmlTitle = `<div class="detail-title">
     <h3>${portfolioDetail.name}</h3>
   </div>`;
   const htmlText = `<div class="detail-text">
     <p>${portfolioDetail.text}</p>
   </div>`
+
+  if (slug === 'angel-skin-clinic') htmlImage = ''
 
   htmlDetail = htmlImage + htmlTitle + htmlText;
 
